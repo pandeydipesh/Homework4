e85a44a6cfdbc4716497cbbb535ccae6e349d75e
Dipesh Pandey <pandeydipesh@yahoo.com>
2021-09-07 23:45:08 -0700

    first commit

---
+++ src/numbers.java
@@ -0,0 +1,25 @@
+
+public class numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		int startIdx = 0;
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j<=i; j++) {
+				startIdx++;
+				System.out.print(startIdx + " ");
+				
+		    }
+			System.out.println("");
+		}
+
+	}
+		
+}
+
+
+
+
+	
